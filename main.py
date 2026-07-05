21a31a35526df8dbfd629390e096696785e963de
Aniket#0903 <aniketsurv>
2026-07-05 20:17:27 +0000

    build image

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from fastapi import FastAPI
+from routers.user import router as user_router
+
+app = FastAPI(title="My First FastAPI App")
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to FastAPI"}
+
+app.include_router(user_router)
---
+++ routers/user.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users"]
+)
+
+@router.get("/")
+def get_users():
+    return [
+        {"id": 1, "name": "Aniket"},
+        {"id": 2, "name": "Rahul"}
+    ]
+
+@router.get("/{user_id}")
+def get_user(user_id: int):
+    return {
+        "id": user_id,
+        "name": "Aniket"
+    }
